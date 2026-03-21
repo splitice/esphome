12693e754d28d1c8e7b6c3acaea13a38d137e4a4
Mathew <mat999@gmail.com>
2026-03-21 13:12:08 +1100

    Update air_conditioner.cpp

--- esphome/components/midea_xye/air_conditioner.cpp
+++ esphome/components/midea_xye/air_conditioner.cpp
@@ -125,7 +125,7 @@ uint8_t AirConditioner::adjust_target_temperature(float target_temperature) cons
   }
 
   if (this->mode == ClimateMode::CLIMATE_MODE_HEAT) {
-    adjusted_target_temperature = ceilf(adjusted_target_temperature - 1);
+    adjusted_target_temperature = ceilf(adjusted_target_temperature - 6);
   } else {
     adjusted_target_temperature = floorf(adjusted_target_temperature);
   }
@@ -141,7 +141,7 @@ float AirConditioner::read_target_temperature(uint8_t target_temperature, bool f
     ret = static_cast<float>(target_temperature & 0xBF);
   }
   if (this->mode == ClimateMode::CLIMATE_MODE_HEAT) {
-    ret += 1;
+    ret += 6;
   }
   return ret;
 }
